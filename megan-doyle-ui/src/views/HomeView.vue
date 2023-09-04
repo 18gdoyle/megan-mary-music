05c20629328a2915839b5c83aad7f61dcd6f962c
Greg Doyle <83316865+18gdoyle@users.noreply.github.com>
2023-09-04 03:32:50 -0400

    added sound

--- megan-doyle-ui/src/views/HomeView.vue
+++ megan-doyle-ui/src/views/HomeView.vue
@@ -4,6 +4,11 @@ import Monkey from '../components/Monkey.vue'
 
 <template>
   <div class="monkeyPlayground" id="monkeyPlayground">
+    <audio id="myAudio">
+      <source src="../assets/cyclone clipped.mp3" type="audio/mpeg">
+      Your browser does not support the audio element.
+    </audio>
+
     <h1>{{ monkeyCount }}</h1>
     <Monkey :id="'monkey1'" @monkeyKilled="monkeyCount = monkeyCount - 1" />
     <Monkey :id="'monkey2'" @monkeyKilled="monkeyCount = monkeyCount - 1" />
@@ -18,10 +23,17 @@ export default {
   data() {
     return {
       monkeyCount: 5,
+      firstClick: true
     }
   },
   mounted() {
-
+    var x = document.getElementById("myAudio"); 
+    window.addEventListener('click', () => {
+      if (this.firstClick) {
+        x.play();
+        this.firstClick = false;
+      }
+    })
   },
   methods: {
 
