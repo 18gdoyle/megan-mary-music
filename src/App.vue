a8ad00333acf9e625025a52c2afea4c56f1abbc6
Greg Doyle <83316865+18gdoyle@users.noreply.github.com>
2024-03-22 15:18:37 -0400

    reconfig

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import NavBar from '@/components/NavBar.vue'
+import MovingGradient from '@/components/MovingGradient.vue';
 </script>
 
 <template>
   <NavBar />
+  <MovingGradient></MovingGradient>
   <RouterView />
 </template>
+
+<style scoped>
+</style>
---
+++ src/components/MovingGradient.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="backgroundOverlay"></div>
+    <section style="position: fixed; height: 100%; width: 100%; z-index: -10; top: 0;">
+      <div id="bg-wrap">
+        <svg viewBox="0 0 100 100" preserveAspectRatio="xMidYMid slice">
+          <defs>
+            <radialGradient id="Gradient1" cx="50%" cy="50%" fx="0.441602%" fy="50%" r=".5">
+              <animate attributeName="fx" dur="68s" values="0%;3%;0%" repeatCount="indefinite"></animate>
+              <stop offset="0%" stop-color="rgba(255,0,0,1)"></stop>
+              <stop offset="100%" stop-color="rgba(255,0,0, 0)"></stop>
+            </radialGradient>
+            <radialGradient id="Gradient2" cx="50%" cy="50%" fx="2.68147%" fy="50%" r=".5">
+              <animate attributeName="fx" dur="48s" values="0%;3%;0%" repeatCount="indefinite"></animate>
+              <stop offset="0%" stop-color="rgba(255,0,0,1)"></stop>
+              <stop offset="100%" stop-color="rgba(255,0,0, 0)"></stop>
+            </radialGradient>
+          </defs>
+          <rect x="13.744%" y="1.18473%" width="100%" height="100%" fill="url(#Gradient1)"
+            transform="rotate(334.41 50 50)">
+            <animate attributeName="x" dur="40s" values="25%;0%;25%" repeatCount="indefinite"></animate>
+            <animate attributeName="y" dur="21s" values="0%;25%;0%" repeatCount="indefinite"></animate>
+            <animateTransform attributeName="transform" type="rotate" from="0 50 50" to="360 50 50" dur="14s"
+              repeatCount="indefinite"></animateTransform>
+          </rect>
+          <rect x="-2.17916%" y="35.4267%" width="100%" height="100%" fill="url(#Gradient2)"
+            transform="rotate(255.072 50 50)">
+            <animate attributeName="x" dur="46s" values="-25%;0%;-25%" repeatCount="indefinite"></animate>
+            <animate attributeName="y" dur="24s" values="0%;50%;0%" repeatCount="indefinite"></animate>
+            <animateTransform attributeName="transform" type="rotate" from="0 50 50" to="360 50 50" dur="24s"
+              repeatCount="indefinite"></animateTransform>
+          </rect>
+        </svg>
+      </div>
+    </section>
+</template>
+
+<style scoped>
+.backgroundOverlay {
+  position: fixed;
+  top: 0;
+  height: 100%;
+  width: 100%;
+  background-color: rgba(0, 0, 0, .35);
+  backdrop-filter: blur(200px);
+  z-index: -9;
+}
+</style>
